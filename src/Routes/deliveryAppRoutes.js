c86dc78d20b9643f667ca9554ae5277d81560b30
rakesh9531 <rakesh.rk9531@gmail.com>
2026-01-31 05:04:15 +0530

    otp

--- src/Routes/deliveryAppRoutes.js
+++ src/Routes/deliveryAppRoutes.js
@@ -8,11 +8,15 @@ router.post('/login', deliveryAppController.login);
 
 // 2. Fetch Assigned Orders
 router.get('/my-orders', auth, deliveryAppController.getMyOrders);
-
+router.post('/send-otp', auth, deliveryAppController.sendDeliveryOTP);
 // 3. Start Delivery (Generates OTP)
 router.post('/start-delivery', auth, deliveryAppController.startDelivery);
 
 // 4. Verify OTP & Complete (Handles Cash/Online Payment)
 router.post('/complete-delivery', auth, deliveryAppController.completeDelivery);
+
+
+router.get('/stats', auth, deliveryAppController.getAgentStats);
+router.post('/cancel-assignment', auth, deliveryAppController.cancelAssignment);
 
 module.exports = router;
